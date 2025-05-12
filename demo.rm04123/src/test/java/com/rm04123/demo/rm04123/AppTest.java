2942e7454b759d71304216057b28af9ad593461d
rm04123 <rm04123@ues.edu.sv>
2025-05-12 03:01:09 -0600

    My first commit

---
+++ demo.rm04123/src/test/java/com/rm04123/demo/rm04123/AppTest.java
@@ -0,0 +1,21 @@
+package com.rm04123.demo.rm04123;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() 
+    {
+        assertTrue(true);
+    }
+}
